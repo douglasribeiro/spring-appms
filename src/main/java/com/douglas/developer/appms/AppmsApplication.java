992475e24f580df2412c76ad4107270a50dca9b6
Douglas Ribeiro <douglas.silva@meta.com.br>
2021-05-24 18:36:05 -0300

    Projeto criado

---
+++ src/main/java/com/douglas/developer/appms/AppmsApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.developer.appms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/developer/appms/AppmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.developer.appms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
