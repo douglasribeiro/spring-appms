8360e8c18d52c0024b4ac235ffb70ff4d60f48e2
Douglas Ribeiro <douglas.silva@meta.com.br>
2021-05-26 14:19:58 -0300

    Categorias listando todas

--- src/main/java/com/douglas/developer/appms/resources/CategoriaResource.java
+++ src/main/java/com/douglas/developer/appms/resources/CategoriaResource.java
@@ -1,8 +1,9 @@
 package com.douglas.developer.appms.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.douglas.developer.appms.domain.Categoria;
+import com.douglas.developer.appms.dto.CategoriaDTO;
 import com.douglas.developer.appms.services.CategoriaService;
 
 @RestController
@@ -52,6 +54,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<CategoriaDTO>> todas() {
+		List<Categoria> lista = service.buscaTodas();
+		List<CategoriaDTO> listDto = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);	
 		
 	}
 }
--- src/main/java/com/douglas/developer/appms/services/CategoriaService.java
+++ src/main/java/com/douglas/developer/appms/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.douglas.developer.appms.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,5 +45,9 @@ public class CategoriaService {
 			throw new DataIntegratyException("Não possivel excluir uma Categoria que contem produtos.");
 		}
 		
+	}
+	
+	public List<Categoria> buscaTodas(){
+		return repository.findAll();
 	}
 }
